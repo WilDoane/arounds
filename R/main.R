5c04dfbae2d03eae70697416153934a2843e483d
William Doane <wdoane@ida.org>
2022-06-23 15:08:50 -0400

    add wrap library()

--- R/main.R
+++ R/main.R
@@ -14,3 +14,27 @@ prerender <- function() {
 
   unlink(path)
 }
+
+library_it <- function() {
+  current_row <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[[1]]
+
+  ranges <- rstudioapi::document_range(
+    c(current_row, 0),
+    c(current_row, Inf)
+  )
+
+  rstudioapi::setSelectionRanges(ranges)
+
+  source <- trimws(rstudioapi::selectionGet())
+
+  target <- rstudioapi::selectionSet(paste0("library(", source, ")"))
+
+  ranges <- rstudioapi::document_range(
+    c(current_row + 1, Inf),
+    c(current_row + 1, Inf)
+  )
+
+  rstudioapi::setSelectionRanges(ranges)
+
+}
+
